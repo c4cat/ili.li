929bcdebbc487e6aa77d3bb42d779a83add87074
mrc <313187850@qq.com>
2014-11-26 00:30:59 +0800

    2014年11月26日00:30:57
    Signed-off-by: mrc <313187850@qq.com>

---
+++ common.js
@@ -0,0 +1,9 @@
+// MRC
+// common.js
+// 2014-11-25 23:01:54
+//
+
+function Wh(){ //get window width and height
+	this.width = $(window).width();
+	this.height = $(window).height();
+}
--- js/bg.js
+++ js/bg.js
@@ -1,3 +1,46 @@
-console.log('bg');
+// MRC
+// background
+// ver 0.0.1
+// 2014-11-24 21:43:03
 
 
+
+var window_width = window.innerWidth,
+    window_height = window.innerHeight,
+    bg_row = Math.ceil(window_width/101),
+    bg_col = Math.ceil(window_height/101);
+
+var stage = new Kinetic.Stage({
+    width: window_width,
+    height: window_height,
+    container: container
+});
+
+var BgLayer = new Kinetic.Layer();
+
+for(var i=0;i<bg_row;i++){
+    for(var j=0;j<bg_col;j++){
+        var bgBox = new Kinetic.Rect({
+            x:i*101,
+            y:j*101,
+            width:100,
+            height:100,
+            strokeWidth:1,
+            cornerRadius:5,
+            fill:'black',
+            stroke:'red'
+        });
+        bgBox.on('click', function() {
+            console.log(this.x());
+            this.opacity(0.5);
+            BgLayer.draw();
+        },true);
+        BgLayer.add(bgBox);
+    }
+}
+
+// BgLayer.on('contentMouseover', function() {
+//     console.log(123);
+//     });
+
+stage.add(BgLayer);
